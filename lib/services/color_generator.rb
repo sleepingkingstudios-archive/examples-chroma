b5650a8ed8568635cad9e81a0a49b93be44ef3b7
sleepingkingstudios <merlin@sleepingkingstudios.com>
2014-08-26 15:46:56 -0400

    Add logging to ColorGenerator, rake task.

--- lib/services/color_generator.rb
+++ lib/services/color_generator.rb
@@ -4,6 +4,8 @@ class ColorGenerator
   class << self
     def generate!
       if Color.count < 1000
+        Rails.logger.info 'Generating color...'
+
         Color.create! :red => rand(256), :green => rand(256), :blue => rand(256)
       end # if
     end # class method generate
--- lib/tasks/colors.rake
+++ lib/tasks/colors.rake
@@ -4,6 +4,8 @@ require 'services/color_generator'
 
 namespace :colors do
   task :generate_colors => :environment do
+    Rails.logger.info 'Running `rake colors:generate_colors`...'
+
     ColorGenerator.generate!
 
     19.times do
